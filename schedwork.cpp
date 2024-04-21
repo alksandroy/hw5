aac6ea28d5a6bfdd1a72a165cd012615c489f24c
Alex Wan <wana@usc.edu>
2024-04-21 06:00:54 +0000

    Removed tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,11 +1,11 @@
 #ifndef RECCHECK
-#include <set>
-#include <iostream>
+#include <algorithm>
 #include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
-#include <map>
-#include <algorithm>
 // add or remove necessary headers as you please
 
 #endif
@@ -14,32 +14,64 @@
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
+// a constant that can be used to indicate an INVALID
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-
 
 // Add your implementation of schedule() and other helper functions here
 
-bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
-)
-{
-    if(avail.size() == 0U){
-        return false;
+bool findSchedule(const AvailabilityMatrix& avail,
+                  const size_t dailyNeed,
+                  const size_t maxShifts,
+                  vector<size_t>& workerNumassigned,
+                  DailySchedule& sched,
+                  size_t day,
+                  size_t slot) {
+    if (day == avail.size()) {
+        return true;
     }
-    sched.clear();
-    // Add your code below
 
+    if (slot == dailyNeed) {
+        return findSchedule(avail, dailyNeed, maxShifts, workerNumassigned,
+                            sched, day + 1, 0);
+    }
 
+    size_t firstWorker = 0;
+    if (slot > 0) {
+        firstWorker = sched[day][slot - 1] + 1;
+    }
 
+    for (size_t worker = firstWorker; worker < avail[day].size(); worker++) {
+        if (avail[day][worker] && workerNumassigned[worker] < maxShifts) {
+            workerNumassigned[worker]++;
+            sched[day][slot] = worker;
 
+            if (findSchedule(avail, dailyNeed, maxShifts, workerNumassigned,
+                             sched, day, slot + 1)) {
+                return true;
+            }
+            workerNumassigned[worker]--;
+            sched[day][slot] = INVALID_ID;
+
+        }
+    }
+    return false;
 }
 
+bool schedule(const AvailabilityMatrix& avail,
+              const size_t dailyNeed,
+              const size_t maxShifts,
+              DailySchedule& sched) {
+    if (avail.size() == 0U) {
+        return false;
+    }
+    vector<size_t> workerNumassigned(avail[0].size(), 0);
+    sched.clear();
+    sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+    // Add your code below
+    return findSchedule(avail, dailyNeed, maxShifts, workerNumassigned, sched,
+                        0, 0);
+}
--- wordle.cpp
+++ wordle.cpp
@@ -2,27 +2,75 @@
 // For debugging
 #include <iostream>
 // For std::remove
-#include <algorithm> 
+#include <algorithm>
 #include <map>
 #include <set>
 #endif
 
-#include "wordle.h"
 #include "dict-eng.h"
+#include "wordle.h"
 using namespace std;
 
-
 // Add prototypes of helper functions here
-
+void generateSatisfiedWords(string current,
+                            set<string>& answers,
+                            const string& in,
+                            string floating,
+                            size_t freedom);
 
 // Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
 
+std::set<std::string> wordle(const std::string& in,
+                             const std::string& floating,
+                             const std::set<std::string>& dict) {
+    set<string> endlishWords, answers;
+    size_t freedom = 0;
+    for (char c : in)
+        if (c == '-')
+            freedom++;
+    generateSatisfiedWords("", endlishWords, in, floating, freedom);
+    for (string word : endlishWords) {
+        if (dict.find(word) != dict.end()) {
+            // cerr << word << endl;
+            answers.insert(word);
+        }
+    }
+    return answers;
+}
+
+void generateSatisfiedWords(string current,
+                            set<string>& answers,
+                            const string& in,
+                            string floating,
+                            size_t freedom) {
+    // cerr << in << " " << current << (bool)(current.size() == in.size()) <<
+    // endl;
+    if (current.size() == in.size()) {
+        if (floating == "")
+            answers.insert(current);
+        return;
+    }
+
+    if (freedom < floating.size())
+        return;
+
+    if (in[current.size()] != '-') {
+        current.push_back(in[current.size()]);
+        generateSatisfiedWords(current, answers, in, floating, freedom);
+    } else {
+        for (char c = 'a'; c <= 'z'; c++) {
+            current.push_back(c);
+            string newFloating = floating;
+
+            for (size_t j = 0; j < floating.size(); j++)
+                if (floating[j] == c) {
+                    newFloating.erase(j, 1);
+                    break;
+                }
+            generateSatisfiedWords(current, answers, in, newFloating, freedom - 1);
+            current.pop_back();
+        }
+    }
 }
 
 // Define any helper functions here
